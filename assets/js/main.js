9e5d6ad89ddcaf8da48ffa1be3ef2a73f674ab10
Fizze Miragayeva <miragayevafizze@gmail.com>
2025-04-25 01:50:27 +0400

    end

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,21 @@
 document.querySelectorAll('.dropdown').forEach(dropdown => {
-    // Dropdown-a klik edildikdə
-    dropdown.addEventListener('click', function (e) {
-        e.stopPropagation(); // Klikin yayılmasını dayandırır
 
-        // Əvvəlcə bütün dropdown-ları bağla
+    dropdown.addEventListener('click', function (e) {
+        e.stopPropagation();
+
         document.querySelectorAll('.dropdown').forEach(d => {
-            // Əgər o dropdown artıq açıqdırsa, onu bağla
+           
             if (d !== this) {
                 d.classList.remove('open');
             }
         });
 
-        // Bu dropdown-un açılmasını təmin et
+        
         this.classList.toggle('open');
     });
 });
 
-// Sayfa daxilində başqa yerlərdə klik edildikdə dropdown-u bağla
+
 window.addEventListener('click', function() {
     document.querySelectorAll('.dropdown').forEach(dropdown => {
         dropdown.classList.remove('open');
@@ -25,12 +24,11 @@ window.addEventListener('click', function() {
 
 
 
-// Yalnız scroll hadisəsindən sonra sticky header göstərilsin
+
 window.addEventListener('scroll', function () {
     var mainHeader = document.querySelector('.main-header-area');
     var stickyHeader = document.querySelector('.header-sticky');
 
-    // Əgər səhifənin yeri əsas header-in altından keçibsə, sticky header göstərilsin
     if (window.scrollY > mainHeader.offsetHeight) {
         stickyHeader.classList.add('sticky');
     } else {
@@ -41,17 +39,17 @@ window.addEventListener('scroll', function () {
 
 
 
-// Banner saygac 
+// Banner 
   document.addEventListener("DOMContentLoaded", function () {
     const counters = document.querySelectorAll(".count");
 
     counters.forEach((counter) => {
-      counter.innerText = "0"; // Başlanğıcda sıfır göstər
+      counter.innerText = "0";
 
       const updateCounter = () => {
-        const target = +counter.innerText; // Mövcud dəyər
-        const endValue = +counter.getAttribute("data-target"); // Məqsəd dəyəri
-        const speed = 120; // sürət nə qədər balaca olsa, bir o qədər sürətli
+        const target = +counter.innerText;
+        const endValue = +counter.getAttribute("data-target"); 
+        const speed = 120; 
 
         if (target < endValue) {
           counter.innerText = Math.ceil(target + (endValue - target) / 10);
